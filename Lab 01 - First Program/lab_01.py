88c6143926eb6b4a3a9bb8cb25f0694bdcb28a35
vb99744 <vb99744@email.vccs.edu>
2024-01-27 13:42:26 -0500

    For lab 1 turn in.

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,16 @@
+print("""Hello, my name is Victoria, and I hope you are having a good day.
+I also hope that this is right because I have had to redo this many times because the 
+files would not open back up.""")
+
+print("Thomas Edison once said, \"Our greatest weakness lies in giving up."
+      "\nThe most certain way to succeed is always to just try one more time\".")
+
+print("I like to crochet little animals and read, the most recent book I read was "
+      "called \'Death on the Causeway\', \nit was a murder mystery and is my favorite"
+      " book so far.")
+
+print("I also like travelling and going camping with my family, we have been to Texas,"
+      "Florida, Tennessee, and South Carolina. \nHowever, my favorite place to visit"
+      "is Myrtle Beach because we have been going there every year since I was little.")
+
+print(15 + 4)
