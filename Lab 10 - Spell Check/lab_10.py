a6aff6df3b45a8390f48baa8a3030b084c8d9239
vb99744 <vb99744@email.vccs.edu>
2024-04-15 21:45:44 -0400

    Spell Check

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -1,7 +1,6 @@
 import re
 
-# this function takes in a line of text and returns
-# a list of words in the line
+# this function takes in a line of text and returns a list of words in the line
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
 
